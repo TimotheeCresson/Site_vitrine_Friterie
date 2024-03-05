80f5f88ea70ebf25866b41b4cea699aeebf08085
Cresson Timothée <timotheecresson@hotmail.fr>
2024-03-05 09:58:33 +0100

    changement rôle phpmyadmin pour donnée relationnelle

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ const app = Vue.createApp({
         imageSliders2: [
           { id:1, image: './img/pizza.jpg', caption: 'Salade, tomate, oignon', nameDish:'Américains fricadelle/jambon/saucisses (knacki x2)', price: '6,50 €',  isHovered: false },
           { id:2, image: './img/frite.jpg', caption: 'Salade, tomate, cheddar', nameDish:'Américains nuggets/poulet/merguez/thon/fromage/crabe/cervelas', price: '7,00 €',  isHovered: false },
-          { id:3, image: './img/pizza.jpg', caption: 'salade, tomate, steack', nameDish:'Américains cervelas/mexicanos/brochette/filet américain/steack + fromage (option SFB + 0,50 €)', price: '8,50 €',  isHovered: false },
+          { id:3, image: './img/pizza.jpg', caption: 'salade, tomate, steack', nameDish:'Américains mexicanos/cervelas/brochette/filet américain/steack + fromage (option SFB + 0,50 €)', price: '8,50 €',  isHovered: false },
           { id:4,image: './img/burger.jpg', caption: 'salade, tomate, steack', nameDish:'Américains grizli', price: '9,00 €',  isHovered: false },
           { id:5, image: './img/pizza.jpg', caption: 'salade, tomate, steack', nameDish:'Américains savoyard/montagnard (+ 1,00 €)', price: '7,00 €',  isHovered: false },
           { id:6,image: './img/burger.jpg', caption: 'salade, tomate, steack', nameDish:`Américains texan/ch'ti`, price: '10,50 €',  isHovered: false },
